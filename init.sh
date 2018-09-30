3a64b57715b38919d5774315831142df180aba8e
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-09-30 15:29:02 -0500

    ^C exits init.sh now

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+trap 'exit' SIGINT
 while true; do
       socat PTY,link=$HOME/dev/vmodem0,rawer,wait-slave EXEC:"ssh -p2222 at.freegeekarkansas.org -lroot ssh -lroot 192.168.1.91 'socat open:/dev/ttyUSB0,b9600,rawer -'"
 done
