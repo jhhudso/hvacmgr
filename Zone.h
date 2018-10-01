5a43ffae3af647ffe8ae137fb9de0f7d5d91c928
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-10-01 00:18:23 -0500

    initial import of Zone class from CZII_to_MQTT project

---
+++ Zone.h
@@ -0,0 +1,86 @@
+// Copyright - Jared Gaillard - 2016
+// Copyright (c) 2018 Jared H. Hudson
+// Licensed under the MIT License
+//
+
+#pragma once
+typedef u_int8_t byte;
+
+const float FLOAT_MIN_VALUE = -1000.0;
+
+class Zone {
+
+public:
+	Zone(byte number) :
+			zoneNumber(number) {
+	}
+
+	void setCoolSetpoint(byte value) {
+		if (coolSetpoint != value)
+			modified = true;
+		coolSetpoint = value;
+	}
+
+	byte getCoolSetpoint() {
+		return coolSetpoint;
+	}
+
+	void setHeatSetpoint(byte value) {
+		if (heatSetpoint != value)
+			modified = true;
+		heatSetpoint = value;
+	}
+
+	byte getHeatSetpoint() {
+		return heatSetpoint;
+	}
+
+	void setTemperature(float value) {
+		if (temperature != value)
+			modified = true;
+		temperature = value;
+	}
+
+	float getTemperature() {
+		return temperature;
+	}
+
+	void setHumidity(byte value) {
+		if (humidity != value)
+			modified = true;
+		humidity = value;
+	}
+
+	byte getHumidity() {
+		return humidity;
+	}
+
+	void setDamperPosition(byte value) {
+		if (damperPosition != value)
+			modified = true;
+		damperPosition = value;
+	}
+
+	byte getDamperPosition() {
+		return damperPosition;
+	}
+
+	bool isModified() {
+		return modified;
+	}
+
+	bool setModified(bool value) {
+		modified = value;
+		return true;
+	}
+
+private:
+	byte zoneNumber = -1;
+	bool modified = false;
+
+	byte coolSetpoint = -1;
+	byte heatSetpoint = -1;
+	float temperature = FLOAT_MIN_VALUE;
+	byte humidity = -1;
+	byte damperPosition = -1;
+};
