a0e3176ccba4fe4ab7e575b7389f15c279b23c19
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-10-01 00:16:11 -0500

    added some basic logging

--- hvacmgr.cpp
+++ hvacmgr.cpp
@@ -1,25 +1,50 @@
 // Copyright (c) 2018 Jared H. Hudson
-// Copyright (c) 2016 jwarcd https://github.com/jwarcd/CZII_to_MQTT
 // Licensed under the MIT License
 //
+
 #include <iostream>
 #include <iomanip>
 #include <cstdio>
 #include <sys/time.h>
 #include <vector>
 #include <unistd.h>
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/signal_set.hpp>
 #include "Frame.h"
 #include "HVAC.h"
 
 using namespace std;
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
 
 bool verbose = false;
+void handler(const boost::system::error_code& error, int signal_number) {
+	if (!error) {
+		// A signal occurred.
+	}
+}
 
 int main(int argc, char **argv) {
 	int opt;
 	bool pty = false;
 	u_int32_t baud_rate = 9600;
 	string device = "/dev/ttyUSB0";
+
+	// Construct a signal set registered for process termination.
+	//boost::asio::signal_set signals(boost::asio::io_service, SIGINT, SIGTERM);
+
+	// Start an asynchronous wait for one of the signals to occur.
+	//signals.async_wait(handler);
 
 	while ((opt = getopt(argc, argv, "pb:f:v")) != -1) {
 		switch (opt) {
@@ -43,6 +68,23 @@ int main(int argc, char **argv) {
 			exit(EXIT_FAILURE);
 		}
 	}
+
+	logging::add_file_log(keywords::file_name = "hvacmgr_%N.log",
+			keywords::rotation_size = 10 * 1024 * 1024,
+			keywords::time_based_rotation = sinks::file::rotation_at_time_point(
+					0, 0, 0), keywords::format = "[%TimeStamp%]: %Message%");
+
+	logging::core::get()->set_filter(
+			logging::trivial::severity >= logging::trivial::info);
+	using namespace logging::trivial;
+	src::severity_logger<severity_level> lg;
+
+	BOOST_LOG_SEV(lg, trace)<< "A trace severity message";
+	BOOST_LOG_SEV(lg, debug)<< "A debug severity message";
+	BOOST_LOG_SEV(lg, info)<< "An informational severity message";
+	BOOST_LOG_SEV(lg, warning)<< "A warning severity message";
+	BOOST_LOG_SEV(lg, error)<< "An error severity message";
+	BOOST_LOG_SEV(lg, fatal)<< "A fatal severity message";
 
 	HVAC hvac(device, baud_rate, pty, 4);
 	hvac.listen();
