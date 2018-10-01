16eec65e00a32430ca9f67b4806b1167481b5820
Jared H. Hudson <426967+jhhudso@users.noreply.github.com>
2018-10-01 00:16:52 -0500

    uses dev name hvac0 now and traps SIGINT for exit

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 trap 'exit' SIGINT
+
+test -d $HOME/dev/ || mkdir $HOME/dev/
+chmod 0700 $HOME/dev/
+
 while true; do
-      socat PTY,link=$HOME/dev/vmodem0,rawer,wait-slave EXEC:"ssh -p2222 at.freegeekarkansas.org -lroot ssh -lroot 192.168.1.91 'socat open:/dev/ttyUSB0,b9600,rawer -'"
+      socat PTY,link=$HOME/dev/hvac0,rawer,wait-slave EXEC:"ssh -p2222 at.freegeekarkansas.org -lroot ssh -lroot 192.168.1.91 'socat open:/dev/ttyUSB0,b9600,rawer -'"
 done
